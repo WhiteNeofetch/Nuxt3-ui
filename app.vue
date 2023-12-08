82f8fd8b08f7cf5443c85d4b9923a72c4c5a4653
whiteneofetch <rumordek@gmail.com>
2023-12-08 17:22:24 +0300

    build

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <UAlert title="Heads up!" />
+    <h1>1123</h1>
+  </div>
+</template>
+<style lang="scss">
+h1 {
+  color: white !important;
+}
+</style>
